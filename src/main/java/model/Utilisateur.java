460e1943c0ad19b87b76b17f4de6f3deae4db6e1
NASSIF <michelnassif1994@gmail.com>
2021-10-12 10:57:23 +0200

    Class attributes

--- src/main/java/model/Utilisateur.java
+++ src/main/java/model/Utilisateur.java
@@ -2,4 +2,67 @@ package model;
 
 public class Utilisateur {
 
+protected String nom ;	
+protected String prenom;
+protected int id ;
+protected String mdp;
+
+
+public Utilisateur(String nom, String prenom, int id, String mdp) {
+	super();
+	this.nom = nom;
+	this.prenom = prenom;
+	this.id = id;
+	this.mdp = mdp;
+}
+
+
+public String getNom() {
+	return nom;
+}
+
+
+public void setNom(String nom) {
+	this.nom = nom;
+}
+
+
+public String getPrenom() {
+	return prenom;
+}
+
+
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+
+
+public int getId() {
+	return id;
+}
+
+
+public void setId(int id) {
+	this.id = id;
+}
+
+
+public String getMdp() {
+	return mdp;
+}
+
+
+public void setMdp(String mdp) {
+	this.mdp = mdp;
+}
+
+
+@Override
+public String toString() {
+	return "Utilisateur [nom=" + nom + ", prenom=" + prenom + ", id=" + id + ", mdp=" + mdp + "]";
+} 
+
+
+
+	
 }
