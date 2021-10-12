af4d50a5d5d1b65015cc09d467b20c62f7a97c01
Benjamin Lachiver <benjamin.lachiver@gmail.com>
2021-10-12 11:00:21 +0200

    Creation de plusieurs nouvelles classes

---
+++ src/main/java/model/Article.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Article {
+
+	private String libelle;
+	private double prix;
+	private String description;
+	
+}
---
+++ src/main/java/model/Restaurant.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.List;
+
+public class Restaurant {
+
+	private String nom;
+	private List<Article> carte;
+	
+}
