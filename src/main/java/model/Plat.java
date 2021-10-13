aaa0f11dfb446ca4e1ea15acf91c58ea7e66ff56
Achille Monod <achille.monod&@gmail.com>
2021-10-13 09:53:26 +0200

    ajout classes

---
+++ src/main/java/model/Plat.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Plat extends Article {
+
+	
+	
+	public Plat(double prix, String libelle) {
+		super(prix, libelle);
+
+	}
+
+	public Plat(double prix, String libelle, String descritption) {
+		super(prix, libelle, descritption);
+
+	}
+
+	@Override
+	public String toString() {
+		return "Plat [prix=" + prix + ", libelle=" + libelle + ", descritption=" + descritption + "]";
+	}
+
+	
+}
